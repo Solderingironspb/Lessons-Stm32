4ac7c9ed8d38985a375c00b663f0c804e925c8ad
Олег Волков <68805120+Solderingironspb@users.noreply.github.com>
2021-08-24 12:27:18 +0300

    Update Float_transform.h

--- Float_transform/Float_transform.h
+++ Float_transform/Float_transform.h
@@ -1,13 +1,18 @@
 /*
  * Float_transform.h
  *
+ * v1.1(Исправление ошибки, когда -1.00f < float value < 0.00f)
+ *
  * Простенькая библиотека для разбития float на две части: целую и дробную.
  * Это нужно, чтоб не использовать -u_printf_float в линкере, дабы сэкономить память МК.
  * В библиотеке используется округление.
  * Пример Val = 25.436f. Мы хотим вывести число с 2 знаками после запятой. Получим 25.44
  *
- *  Created on: Aug 18, 2021
+ *  Created on: Aug 24, 2021
  *      Author: Oleg Volkov
+ *
+ *      P.S Хочу сказать огромное спасибо Дмитрию Мачневу за тестирование и подсказки по оптимизации!
+ *      В будущем может еще чего переделаем.
  */
 
 #ifndef INC_FLOAT_TRANSFORM_H_
@@ -15,6 +20,6 @@
 
 #include <main.h>
 
-void Float_transform(float value, uint8_t width, int *integer_number, uint32_t *fractional_number);
+void Float_transform(float value, uint8_t width, uint8_t *sign_number, int *integer_number, uint32_t *fractional_number);
 
 #endif /* INC_FLOAT_TRANSFORM_H_ */
