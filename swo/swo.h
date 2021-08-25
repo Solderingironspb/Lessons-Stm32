b6a36ae9485bba65c3df539115389e0a555030f1
Solderingironspb <olegdizzvolkov@yandex.ru>
2021-08-25 17:02:19 +0300

    SWO_lib_add

---
+++ swo/swo.h
@@ -0,0 +1,16 @@
+/*
+ * SWO.h
+ *
+ *  Created on: Aug 18, 2021
+ *      Author: Oleg Volkov
+ */
+
+#ifndef INC_SWO_H_
+#define INC_SWO_H_
+
+#include <main.h>
+
+int _write(int file, char *ptr, int len);
+
+
+#endif /* INC_SWO_H_ */
